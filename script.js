91cbbc6652f136c8dfb733fa11f02ed5ac199667
Pavel Kovalchuk <pavelkovalchuk0510@gmail.com>
2025-12-07 23:56:52 +0100

    created responsive and modern web design for a exchange rate app, js is coming soon

---
+++ script.js
@@ -0,0 +1,9 @@
+//https://github.com/fawazahmed0/exchange-api
+
+const amountInput = document.querySelector('.amount-one');
+const amountOutput = document.querySelector('.amount-two');
+const currencyOne = document.querySelector('#currency-one');
+const currencyTwo = document.querySelector('#currency-two');
+const swapBtn = document.querySelector('.swap');
+const rateInfo = document.querySelector('.rate-info');
+const calculateBtn = document.querySelector('.calculate');
